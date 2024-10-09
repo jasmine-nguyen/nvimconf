5cb1bbef6bed6030ecd95419f08656d571553c38
Jasmine Nguyen <jasminenguyen@Jasmines-MacBook-Air.local>
2024-10-09 21:37:11 +1100

    Add golangci-lint

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -9,7 +9,8 @@ return {
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettierd,
-        require("none-ls.diagnostics.eslint_d")
+        null_ls.builtins.diagnostics.golangci_lint,
+        require("none-ls.diagnostics.eslint_d"),
       },
     })
 
