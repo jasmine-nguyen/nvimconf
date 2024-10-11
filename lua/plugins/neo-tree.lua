b274d690d9a746263d8a4212208a14013849e642
jasmine-nguyen <jasminel.ngn@gmail.com>
2024-10-11 23:00:55 +1100

    Update neo-tree.lua

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -5,6 +5,7 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
+    "BurntSushi/ripgrep",
   },
   config = function()
     vim.keymap.set("n", "\\", ":Neotree filesystem toggle left<CR>")
@@ -15,6 +16,11 @@ return {
   },
   opts = {
     filesystem = {
+      filtered_items = {
+        visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+        hide_dotfiles = false,
+        hide_gitignored = true,
+      },
       window = {
         mappings = {
           ["\\"] = "close_window",
