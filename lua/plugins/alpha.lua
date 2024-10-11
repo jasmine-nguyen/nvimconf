a06ad00b59042ff5c362b31a80cdc5b9f37940ec
Jasmine Nguyen <jasminenguyen@Jasmines-MacBook-Air.local>
2024-10-11 13:06:34 +1100

    Update dashboard header

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,11 +1,19 @@
-return  {
+return {
   "goolord/alpha-nvim",
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     local startify = require("alpha.themes.startify")
     startify.file_icons.provider = "devicons"
-    require("alpha").setup(
-      startify.config
-    )
+    startify.section.header.val = {
+      [[   _    _                   _            ]],
+      [[  | |  | |                 | |           ]],
+      [[  | |__| | ___ _   _       | | __ _ ___  ]],
+      [[  |  __  |/ _ \ | | |  _   | |/ _` / __| ]],
+      [[  | |  | |  __/ |_| | | |__| | (_| \__ \ ]],
+      [[  |_|  |_|\___|\__, |  \____/ \__,_|___/ ]],
+      [[                __/ |                    ]],
+      [[               |___/                     ]],
+    }
+    require("alpha").setup(startify.config)
   end,
 }
