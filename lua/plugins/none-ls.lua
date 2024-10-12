a7a1f18a1a027febab4e37285edc6e522177c9a4
Jasmine Nguyen <jasminenguyen@Jasmines-MacBook-Air.local>
2024-10-12 19:59:10 +1100

    Refactor none-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,10 +7,10 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
+				require("none-ls.diagnostics.eslint_d"),
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettierd,
 				null_ls.builtins.diagnostics.golangci_lint,
-				require("none-ls.diagnostics.eslint_d"),
 			},
 		})
 
