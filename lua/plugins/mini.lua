e3f6eccab4eec257c9c0436670bc6b6179008e1d
Jasmine Nguyen <jasminenguyen@Jasmines-MacBook-Air.local>
2024-10-12 21:44:07 +1100

    Replace autopairs with mini.pairs

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -3,5 +3,10 @@ return {
 	config = function()
 		require("mini.ai").setup({ n_lines = 500 })
 		require("mini.surround").setup()
+		require("mini.pairs").setup({
+			mappings = {
+				["<"] = { action = "closeopen", pair = "<>", neigh_pattern = "[^\\].", register = { cr = false } },
+			},
+		})
 	end,
 }
