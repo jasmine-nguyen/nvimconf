995b823735d9ac87cfde135f472022483f67bdc2
Jasmine Nguyen <jasminenguyen@Jasmines-MacBook-Air.local>
2024-10-12 22:32:56 +1100

    Add ripgrep as a dependency for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,7 +2,7 @@ return {
 	{
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.8",
-		dependencies = { "nvim-lua/plenary.nvim" },
+		dependencies = { "nvim-lua/plenary.nvim", "BurntSushi/ripgrep" },
 		cmd = { "Telescope" },
 		config = function()
 			local builtin = require("telescope.builtin")
