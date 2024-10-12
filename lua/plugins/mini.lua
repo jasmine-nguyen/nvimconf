8c06e0d97d6f2b00ec3250fef6258733e53c78c1
Jasmine Nguyen <jasminenguyen@Jasmines-MacBook-Air.local>
2024-10-12 22:33:14 +1100

    Replace neo-tree with mini.files

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,12 +1,35 @@
 return {
 	"echasnovski/mini.nvim",
 	config = function()
+		-- MiniAi setup
 		require("mini.ai").setup({ n_lines = 500 })
+
+		-- MiniFiles setup
+		require("mini.files").setup({
+			use_as_default_explorer = true,
+			windows = {
+				max_number = math.huge,
+				preview = false,
+				width_focus = 30,
+				width_nofocus = 20,
+				width_preview = 25,
+			},
+		})
+		vim.keymap.set("n", "<leader>\\", "<cmd>lua MiniFiles.open()<CR>", { desc = "File explorer" })
+
+		-- MiniSurround setup
 		require("mini.surround").setup()
+
+		-- MiniPairs setup
 		require("mini.pairs").setup({
 			mappings = {
 				["<"] = { action = "closeopen", pair = "<>", neigh_pattern = "[^\\].", register = { cr = false } },
 			},
+		})
+
+		-- MiniIndentscope setup
+		require("mini.indentscope").setup({
+			symbol = "┋",
 		})
 	end,
 }
