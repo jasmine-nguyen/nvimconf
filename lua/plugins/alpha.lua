133b1babaf04d0692530f671def27e37d2a22b07
Jasmine Nguyen <jasminenguyen@Jasmines-MacBook-Air.local>
2024-10-14 19:15:42 +1100

    Update greeter header

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -3,6 +3,16 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		local startify = require("alpha.themes.startify")
+		startify.section.header.val = {
+			[[ _   _                ___           _ ]],
+			[[| | | |              |_  |         | |]],
+			[[| |_| | ___ _   _      | | __ _ ___| |]],
+			[[|  _  |/ _ \ | | |     | |/ _` / __| |]],
+			[[| | | |  __/ |_| | /\__/ / (_| \__ \_|]],
+			[[\_| |_/\___|\__, | \____/ \__,_|___(_)]],
+			[[             __/ |                    ]],
+			[[            |___/                     ]],
+		}
 		startify.file_icons.provider = "devicons"
 		require("alpha").setup(startify.config)
 	end,
