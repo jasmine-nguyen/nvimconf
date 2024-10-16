e49cd1f1b16de10f416ea057c28d0466b9572a63
jasmine-nguyen <jasminel.ngn@gmail.com>
2024-10-17 07:59:28 +1100

    Update vim-options.lua

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,92 +1,98 @@
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+-- set <space> as the leader key
+-- see `:help mapleader`
+--  note: must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.opt.termguicolors = true
 
--- Set to true if you have a Nerd Font installed and selected in the terminal
+-- set to true if you have a nerd font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
--- Make line numbers default
+-- make line numbers default
 vim.opt.number = true
 
--- Don't show the mode, since it's already in the status line
+-- don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
--- Enable mouse mode, can be useful for resizing splits for example!
+-- enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = "a"
 
--- Enable break indent
+-- enable break indent
 vim.opt.breakindent = true
 
--- Save undo history
+-- save undo history
 vim.opt.undofile = true
 
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+-- case-insensitive searching unless \c or one or more capital letters in the search term
 vim.opt.smartcase = true
 
--- Keep signcolumn on by default
+-- keep signcolumn on by default
 vim.opt.signcolumn = "yes"
 
--- Decrease update time
+-- decrease update time
 vim.opt.updatetime = 250
 
--- Decrease mapped sequence wait time
--- Displays which-key popup sooner
+-- decrease mapped sequence wait time
+-- displays which-key popup sooner
 vim.opt.timeoutlen = 300
 
--- Configure how new splits should be opened
+-- configure how new splits should be opened
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
--- Preview substitutions live, as you type!
+-- preview substitutions live, as you type!
 vim.opt.inccommand = "split"
 
--- Show which line your cursor is on
+-- show which line your cursor is on
 vim.opt.cursorline = true
 
--- The amount of space on screen a Tab character can occupy
+-- the amount of space on screen a tab character can occupy
 vim.opt.tabstop = 2
 
--- Amount of characters Neovim will use to indent a line
+-- amount of characters neovim will use to indent a line
 vim.opt.shiftwidth = 2
 
--- Controls whether or not Neovim should transform a Tab character to spaces
+-- controls whether or not neovim should transform a tab character to spaces
 vim.opt.expandtab = false
 
--- Minimal number of screen lines to keep above and below the cursor.
+-- minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
--- [[ Basic Keymaps ]]
-vim.keymap.set("n", "<leader>w", "<cmd>write<CR>", { desc = "Save" })
-vim.keymap.set("n", "<leader>q", "<cmd>:q<CR>", { desc = "Quit" })
--- Modify x and X to delete text without changing the internal registers
-vim.keymap.set({ "n", "x" }, "x", '"_x')
-vim.keymap.set({ "n", "x" }, "X", '"_d')
--- Select all text in current buffer
-vim.keymap.set("n", "<leader>a", ":keepjumps normal! ggVG<cr>")
---  See `:help wincmd` for a list of all window commands
-vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", ":wincmd k<CR>", { desc = "Move focus to the upper window" })
+-- when on, lines longer than the width of the window will wrap and displaying continues on the next line
+vim.opt.wrap = true
 
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
+-- maximum width of text that is being inserted.  a longer line will be broken after white space to get this width.
+vim.opt.textwidth = 80
+
+-- [[ basic keymaps ]]
+vim.keymap.set("n", "<leader>w", "<cmd>write<cr>", { desc = "save" })
+vim.keymap.set("n", "<leader>q", "<cmd>:q<cr>", { desc = "quit" })
+-- modify x and x to delete text without changing the internal registers
+vim.keymap.set({ "n", "x" }, "x", '"_x')
+vim.keymap.set({ "n", "x" }, "x", '"_d')
+-- select all text in current buffer
+vim.keymap.set("n", "<leader>a", ":keepjumps normal! ggvg<cr>")
+--  see `:help wincmd` for a list of all window commands
+vim.keymap.set("n", "<c-h>", ":wincmd h<cr>", { desc = "move focus to the left window" })
+vim.keymap.set("n", "<c-l>", ":wincmd l<cr>", { desc = "move focus to the right window" })
+vim.keymap.set("n", "<c-j>", ":wincmd j<cr>", { desc = "move focus to the lower window" })
+vim.keymap.set("n", "<c-k>", ":wincmd k<cr>", { desc = "move focus to the upper window" })
+
+-- highlight when yanking (copying) text
+--  try it with `yap` in normal mode
+--  see `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("textyankpost", {
+	desc = "highlight when yanking (copying) text",
 	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
 	callback = function()
 		vim.highlight.on_yank()
 	end,
 })
 
--- Sync clipboard between OS and Neovim.
---  Schedule the setting after `UiEnter` because it can increase startup-time.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
+-- sync clipboard between os and neovim.
+--  schedule the setting after `uienter` because it can increase startup-time.
+--  remove this option if you want your os clipboard to remain independent.
+--  see `:help 'clipboard'`
 vim.schedule(function()
 	vim.opt.clipboard = "unnamedplus"
 end)
