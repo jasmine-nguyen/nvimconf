e9afa08cf6626bb17092cb7aa11ed255f8e107f3
Jasmine Nguyen <jasminenguyen@Jasmines-MacBook-Air.local>
2024-10-19 12:57:39 +1100

    Simplify toggleterm config

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,14 +3,10 @@ return {
 	version = "*",
 	opts = {
 		size = 20,
-		open_mapping = [[<c-/>]],
-		hide_numbers = true, -- hide the number column in toggleterm buffers
-		shade_filetypes = {},
-		shade_terminals = true,
-		shading_factor = 2, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
+		open_mapping = "<leader>tt",
 		start_in_insert = true,
 		insert_mappings = true, -- whether or not the open mapping applies in insert mode
-		persist_size = false,
+		terminal_mappings = true,
 		direction = "float",
 		close_on_exit = true, -- close the terminal window when the process exits
 		shell = nil, -- change the default shell
@@ -22,21 +18,5 @@ return {
 				background = "Normal",
 			},
 		},
-		winbar = {
-			enabled = true,
-			name_formatter = function(term) --  term: Terminal
-				return term.count
-			end,
-		},
-		config = function()
-			local opts = { noremap = true, silent = true }
-			function _G.set_terminal_keymaps()
-				vim.api.nvim_buf_set_keymap(0, "t", "<ESC>", [[<C-/><C-n>]], opts)
-				vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-/><C-n><C-W>h]], opts)
-				vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-/><C-n><C-W>j]], opts)
-				vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-/><C-n><C-W>k]], opts)
-				vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-/><C-n><C-W>l]], opts)
-			end
-		end,
 	},
 }
