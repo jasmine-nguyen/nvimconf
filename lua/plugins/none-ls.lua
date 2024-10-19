2813450305eb87bdb280f03bebc952633b6ae444
Jasmine Nguyen <jas@Jasmines-MacBook-Air.local>
2024-10-19 21:39:07 +1100

    Add protolint/gofmt linter and formatter

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -8,9 +8,11 @@ return {
 		null_ls.setup({
 			sources = {
 				require("none-ls.diagnostics.eslint_d"),
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettierd,
 				null_ls.builtins.diagnostics.golangci_lint,
+				null_ls.builtins.diagnostics.protolint,
+				null_ls.builtins.formatting.gofmt,
+				null_ls.builtins.formatting.prettierd,
+				null_ls.builtins.formatting.stylua,
 			},
 		})
 
