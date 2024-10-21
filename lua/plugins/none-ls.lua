a112e2b2a538396d11fde6cedb05ab15dcab7c84
jasmine-nguyen <jasminel.ngn@gmail.com>
2024-10-21 22:00:08 +1100

    Add protolint and buf

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -8,8 +8,9 @@ return {
 		null_ls.setup({
 			sources = {
 				require("none-ls.diagnostics.eslint_d"),
-				null_ls.builtins.diagnostics.golangci_lint,
 				null_ls.builtins.diagnostics.protolint,
+				null_ls.builtins.diagnostics.golangci_lint,
+				null_ls.builtins.formatting.buf,
 				null_ls.builtins.formatting.gofmt,
 				null_ls.builtins.formatting.prettierd,
 				null_ls.builtins.formatting.stylua,
