9eee314a9ae9401b55af3f13a6d105ee32280765
Jasmine Nguyen <jas@Jasmines-MacBook-Air.local>
2024-11-01 19:24:35 +1100

    Update blink config

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -3,19 +3,21 @@ return {
 	event = { "LspAttach", "InsertCharPre" },
 	version = "v0.*",
 	opts = {
+		accept = { auto_brackets = { enabled = true } },
 		highlight = {
 			use_nvim_cmp_as_default = true,
 		},
+		keymap = "super-tab",
 		nerd_font_variant = "mono",
 		windows = {
 			autocomplete = {
 				border = "single",
 			},
 			documentation = {
+				auto_show = true,
 				border = "single",
 			},
 		},
-		accept = { auto_brackets = { enabled = true } },
 		-- trigger = { signature_help = { enabled = true } },
 	},
 }
