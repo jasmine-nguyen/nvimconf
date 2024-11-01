1d965b2b6c83b1b89f99cae3be14a895e2d2cf11
Jasmine Nguyen <jas@Jasmines-MacBook-Air.local>
2024-11-01 19:26:22 +1100

    Update neotree toggle keymapping

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -8,9 +8,45 @@ return {
 		"MunifTanjim/nui.nvim",
 		"BurntSushi/ripgrep",
 	},
-	keys = {
-		{ "\\", ":Neotree reveal<CR>", desc = "neotree reveal", silent = true },
-	},
+	keys = function()
+		local find_buffer_by_type = function(type)
+			for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+				local ft = vim.api.nvim_buf_get_option(buf, "filetype")
+				if ft == type then
+					return buf
+				end
+			end
+			return -1
+		end
+		local toggle_neotree = function(toggle_command)
+			if find_buffer_by_type("neo-tree") > 0 then
+				require("neo-tree.command").execute({ action = "close" })
+			else
+				toggle_command()
+			end
+		end
+
+		return {
+			{
+				"\\",
+				function()
+					toggle_neotree(function()
+						require("neo-tree.command").execute({ action = "focus", reveal = true, dir = vim.uv.cwd() })
+					end)
+				end,
+				desc = "Toggle Explorer (cwd)",
+			},
+			{
+				"\\",
+				function()
+					toggle_neotree(function()
+						require("neo-tree.command").execute({ action = "focus", reveal = true })
+					end)
+				end,
+				desc = "Toggle Explorer (root)",
+			},
+		}
+	end,
 	opts = {
 		filesystem = {
 			filtered_items = {
