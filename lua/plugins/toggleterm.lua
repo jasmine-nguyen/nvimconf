d6a0a42aa90241186d6a47a0b7e3a2c5cbbc3d4a
Jasmine Nguyen <jas@Jasmines-MacBook-Air.local>
2024-11-01 22:35:27 +1100

    Update open mapping to fix spacebar delay

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -5,7 +5,7 @@ return {
 	opts = {
 		autochdir = true,
 		size = 20,
-		open_mapping = "<leader>tt",
+		open_mapping = [[<c-/>]],
 		start_in_insert = true,
 		insert_mappings = true, -- whether or not the open mapping applies in insert mode
 		terminal_mappings = true,
