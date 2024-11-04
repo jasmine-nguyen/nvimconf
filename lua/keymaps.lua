ebd5725d545ee3af0bbbca94a446dff9adf17431
Jasmine Nguyen <jas@Jasmines-MacBook-Air.local>
2024-11-04 12:33:03 +1100

    Update keymap desc

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,6 +1,6 @@
 -- [[ basic keymaps ]]
-vim.keymap.set("n", "<leader>q", "<cmd>:q<cr>", { desc = "quit", noremap = true })
-vim.keymap.set("n", "<leader>w", "<cmd>:w<cr>", { desc = "save", noremap = true })
+vim.keymap.set("n", "<leader>q", "<cmd>:q<cr>", { desc = "quit buffer", noremap = true })
+vim.keymap.set("n", "<leader>w", "<cmd>:w<cr>", { desc = "save buffer", noremap = true })
 vim.keymap.set("i", "jj", "<esc>", { desc = "escape", noremap = true })
 vim.keymap.set(
 	"n",
