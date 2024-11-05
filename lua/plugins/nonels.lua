a9c8ecd19372a09dc6af54cf4ef6812a57672178
Jasmine Nguyen <jas@Jasmines-MacBook-Air.local>
2024-11-05 11:44:21 +1100

    Setup format on save

--- lua/plugins/nonels.lua
+++ lua/plugins/nonels.lua
@@ -5,19 +5,32 @@ return {
 		"nvimtools/none-ls-extras.nvim",
 	},
 	config = function()
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
 				require("none-ls.diagnostics.eslint_d"),
 				null_ls.builtins.diagnostics.protolint,
 				null_ls.builtins.diagnostics.golangci_lint.with({
-					args = {"--allow-parallel-runners"}
+					args = { "--allow-parallel-runners" },
 				}),
 				null_ls.builtins.formatting.buf,
 				null_ls.builtins.formatting.gofmt,
 				null_ls.builtins.formatting.prettierd,
 				null_ls.builtins.formatting.stylua,
 			},
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({ async = false })
+						end,
+					})
+				end
+			end,
 		})
 	end,
 }
