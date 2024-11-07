f4af5edbc9cfdd7afb54a040e33af776b5aaa0dd
Jasmine Nguyen <jas@Jasmines-MacBook-Air.local>
2024-11-07 21:01:30 +1100

    Update toggleterm config

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -4,6 +4,11 @@ return {
 	version = "*",
 	opts = {
 		autochdir = true,
+		highlights = {
+			FloatBorder = {
+				guifg = "#27a1b9",
+			},
+		},
 		size = 20,
 		open_mapping = [[<c-/>]],
 		start_in_insert = true,
@@ -14,11 +19,19 @@ return {
 		shell = nil, -- change the default shell
 		float_opts = {
 			border = "rounded",
+			height = 50,
+			width = 200,
 			winblend = 0,
 			highlights = {
 				border = "Normal",
 				background = "Normal",
 			},
+		},
+		winbar = {
+			enabled = true,
+			name_formatter = function(term) --  term: Terminal
+				return term.name
+			end,
 		},
 	},
 	vim.keymap.set({ "t" }, "<esc>", "<cmd>close<CR>", { noremap = true, silent = true }),
