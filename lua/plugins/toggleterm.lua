8a30587d5e5bee8c739881c0070d8522355ca95b
Jasmine Nguyen <jas@Jasmines-MacBook-Air.local>
2024-11-08 21:36:35 +1100

    Update toggleterm open mapping

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -10,9 +10,9 @@ return {
 			},
 		},
 		size = 20,
-		open_mapping = [[<c-/>]],
+		open_mapping = [[ft]],
 		start_in_insert = true,
-		insert_mappings = true, -- whether or not the open mapping applies in insert mode
+		insert_mappings = false, -- whether or not the open mapping applies in insert mode
 		terminal_mappings = true,
 		direction = "float",
 		close_on_exit = true, -- close the terminal window when the process exits
