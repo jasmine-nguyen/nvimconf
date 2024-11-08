d05de566013c0f3361f8f4107bb8cf7d536c7da0
Jasmine Nguyen <jas@Jasmines-MacBook-Air.local>
2024-11-08 21:36:18 +1100

    Update general keymap

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,4 +1,5 @@
 -- [[ basic keymaps ]]
+vim.keymap.set("n", "<leader>Q", "<cmd>:q!<cr>", { desc = "quit buffer without saving", noremap = true })
 vim.keymap.set("n", "<leader>q", "<cmd>:q<cr>", { desc = "quit buffer", noremap = true })
 vim.keymap.set("n", "<leader>w", "<cmd>:w<cr>", { desc = "save buffer", noremap = true })
 vim.keymap.set("i", "jj", "<esc>", { desc = "escape", noremap = true })
@@ -12,8 +13,8 @@ vim.keymap.set(
 -- modify x and x to delete text without changing the internal registers
 vim.keymap.set({ "n", "x" }, "x", '"_x')
 vim.keymap.set({ "n", "x" }, "x", '"_d')
-vim.keymap.set({ "n", "x" }, "d", '"_x')
-vim.keymap.set({ "n", "x" }, "d", '"_d')
+-- vim.keymap.set({ "n", "x" }, "d", '"_x')
+-- vim.keymap.set({ "n", "x" }, "d", '"_d')
 
 --  see `:help wincmd` for a list of all window commands
 vim.keymap.set("n", "<c-h>", ":wincmd h<cr>", { desc = "move focus to the left window" })
