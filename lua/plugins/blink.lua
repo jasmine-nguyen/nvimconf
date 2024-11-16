5d990773aabe7583d1d57308c7b2ddd0097278ae
Jasmine Nguyen <jas@Jasmines-MacBook-Air.local>
2024-11-16 14:42:50 +1100

    Try copilot with blink

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,28 +1,53 @@
 return {
-	"saghen/blink.cmp",
-	event = { "LspAttach", "InsertCharPre" },
-	version = "v0.*",
-	opts = {
-		accept = { auto_brackets = { enabled = true } },
-		highlight = {
-			use_nvim_cmp_as_default = true,
-		},
-		keymap = { preset = "super-tab" },
-		nerd_font_variant = "mono",
-		windows = {
-			autocomplete = {
-				border = "single",
-			},
-			documentation = {
-				auto_show = true,
-				border = "single",
+	{
+		"saghen/blink.cmp",
+		dependencies = {
+			{
+				"giuxtaposition/blink-cmp-copilot",
 			},
 		},
-		trigger = {
-			completion = {
-				show_in_snippet = false,
+		event = { "LspAttach", "InsertCharPre" },
+		version = "v0.*",
+		opts = {
+			accept = { auto_brackets = { enabled = true } },
+			highlight = {
+				use_nvim_cmp_as_default = true,
 			},
-			signature_help = { enabled = true },
+			keymap = { preset = "super-tab" },
+			nerd_font_variant = "mono",
+			providers = {
+				copilot = {
+					name = "copilot",
+					module = "blink-cmp-copilot",
+				},
+			},
+			windows = {
+				autocomplete = {
+					border = "single",
+				},
+				documentation = {
+					auto_show = true,
+					border = "single",
+				},
+			},
+			trigger = {
+				completion = {
+					show_in_snippet = false,
+				},
+				signature_help = { enabled = true },
+			},
 		},
+	},
+	{
+		"giuxtaposition/blink-cmp-copilot",
+		dependencies = {
+			"copilot.lua",
+		},
+		config = function()
+			require("copilot").setup({
+				suggestion = { enabled = false },
+				panel = { enabled = false },
+			})
+		end,
 	},
 }
