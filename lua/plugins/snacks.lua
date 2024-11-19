ff55042d6222a8fd181e1c1de20b9b3564e2e964
Jasmine Nguyen <jas@Jasmines-MacBook-Air.local>
2024-11-19 19:52:38 +1100

    Try out snacks dashboard

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,6 +3,17 @@ return {
 	priority = 1000,
 	lazy = false,
 	opts = {
+		dashboard = {
+			enabled = true,
+			autokeys = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+			sections = {
+				{ section = "header" },
+				{ icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+				{ icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+				{ icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+				{ section = "startup" },
+			},
+		},
 		lazygit = { enabled = true },
 	},
 	keys = {
