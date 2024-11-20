2e45802120ce0dd3bb20742315d2104d40f91ddb
jasmine-nguyen <jasminel.ngn@gmail.com>
2024-11-20 22:50:14 +1100

    Update key mapping

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -7,13 +7,18 @@ return {
 		local fzftodo = require("todo-comments.fzf")
 		fzflua.setup({
 			-- key mappings
-			vim.keymap.set("n", "fc", fzflua.command_history, { desc = "find command history", noremap = true }),
-			vim.keymap.set("n", "fb", fzflua.buffers, { desc = "find buffers", noremap = true }),
-			vim.keymap.set("n", "ff", fzflua.files, { desc = "find files", noremap = true }),
-			vim.keymap.set("n", "fk", fzflua.keymaps, { desc = "find key mappings", noremap = true }),
-			vim.keymap.set("n", "fg", fzflua.live_grep, { desc = "find by grep", noremap = true }),
-			vim.keymap.set("n", "fo", fzflua.oldfiles, { desc = "find old files", noremap = true }),
-			vim.keymap.set("n", "ft", fzftodo.todo, { desc = "find todos", noremap = true }),
+			vim.keymap.set(
+				"n",
+				"<leader>fc",
+				fzflua.command_history,
+				{ desc = "find command history", noremap = true }
+			),
+			vim.keymap.set("n", "<leader>fb", fzflua.buffers, { desc = "find buffers", noremap = true }),
+			vim.keymap.set("n", "<leader>ff", fzflua.files, { desc = "find files", noremap = true }),
+			vim.keymap.set("n", "<leader>fk", fzflua.keymaps, { desc = "find key mappings", noremap = true }),
+			vim.keymap.set("n", "<leader>fg", fzflua.live_grep, { desc = "find by grep", noremap = true }),
+			vim.keymap.set("n", "<leader>fo", fzflua.oldfiles, { desc = "find old files", noremap = true }),
+			vim.keymap.set("n", "<leader>ft", fzftodo.todo, { desc = "find todos", noremap = true }),
 		})
 	end,
 }
