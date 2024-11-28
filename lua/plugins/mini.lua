6c457a48b5afaa5295d5a62086c09f4ee990bfd4
Jasmine Nguyen <maverick@Jasmines-MacBook-Air.local>
2024-11-28 22:37:38 +1100

    Add keymapping for split

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -26,6 +26,29 @@ return {
 
 		-- MiniFiles setup
 		local minifiles = require("mini.files")
+
+		local map_split = function(buf_id, lhs, direction, close_on_file)
+			local rhs = function()
+				local new_target_window
+				local cur_target_window = require("mini.files").get_explorer_state().target_window
+				if cur_target_window ~= nil then
+					vim.api.nvim_win_call(cur_target_window, function()
+						vim.cmd("belowright " .. direction .. " split")
+						new_target_window = vim.api.nvim_get_current_win()
+					end)
+
+					require("mini.files").set_target_window(new_target_window)
+					require("mini.files").go_in({ close_on_file = close_on_file })
+				end
+			end
+
+			local desc = "Open in " .. direction .. " split"
+			if close_on_file then
+				desc = desc .. " and close"
+			end
+			vim.keymap.set("n", lhs, rhs, { buffer = buf_id, desc = desc })
+		end
+
 		minifiles.setup({
 			vim.keymap.set("n", "\\", function()
 				minifiles.open(nil, false)
@@ -34,6 +57,18 @@ return {
 			vim.keymap.set("n", "<esc>", function()
 				minifiles.close()
 			end, { desc = "close minifiles", noremap = true }),
+
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "MiniFilesBufferCreate",
+				callback = function(args)
+					local buf_id = args.data.buf_id
+
+					map_split(buf_id, "<leader>h", "horizontal", false)
+					map_split(buf_id, "<leader>v", "vertical", false)
+					map_split(buf_id, "<leader>H", "horizontal", true)
+					map_split(buf_id, "<leader>V", "vertical", true)
+				end,
+			}),
 		})
 
 		-- MiniIndentscope setup
