dd2a9cf7004995c7de3ec37dc27496ec9563c9c3
Jasmine Nguyen <maverick@Jasmines-MacBook-Air.local>
2024-12-24 18:15:20 +1100

    Update plugins and blink config

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -9,33 +9,7 @@ return {
 		event = { "LspAttach", "InsertCharPre" },
 		version = "v0.*",
 		opts = {
-			accept = { auto_brackets = { enabled = true } },
-			highlight = {
-				use_nvim_cmp_as_default = true,
-			},
 			keymap = { preset = "super-tab" },
-			nerd_font_variant = "mono",
-			providers = {
-				copilot = {
-					name = "copilot",
-					module = "blink-cmp-copilot",
-				},
-			},
-			windows = {
-				autocomplete = {
-					border = "single",
-				},
-				documentation = {
-					auto_show = true,
-					border = "single",
-				},
-			},
-			trigger = {
-				completion = {
-					show_in_snippet = false,
-				},
-				signature_help = { enabled = true },
-			},
 		},
 	},
 	{
