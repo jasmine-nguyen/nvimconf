49a534da9095161d303a10f2fdd0a849413ed8f8
Jasmine Nguyen <maverick@Jasmines-MacBook-Air.local>
2025-01-13 20:08:49 +1100

    Remove copilot

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,27 +1,10 @@
 return {
 	{
 		"saghen/blink.cmp",
-		dependencies = {
-			{
-				"giuxtaposition/blink-cmp-copilot",
-			},
-		},
 		event = { "LspAttach", "InsertCharPre" },
 		version = "v0.*",
 		opts = {
 			keymap = { preset = "super-tab" },
 		},
-	},
-	{
-		"giuxtaposition/blink-cmp-copilot",
-		dependencies = {
-			"copilot.lua",
-		},
-		config = function()
-			require("copilot").setup({
-				suggestion = { enabled = false },
-				panel = { enabled = false },
-			})
-		end,
 	},
 }
