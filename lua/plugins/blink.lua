d04f83c77df8dbbabd09dab295fb1d4e79999aba
Jasmine Nguyen <maverick@Jasmines-MacBook-Air.local>
2025-01-31 20:33:52 +1100

    Update blink config

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -4,6 +4,10 @@ return {
 		event = { "LspAttach", "InsertCharPre" },
 		version = "v0.*",
 		opts = {
+			completion = {
+				accept = { auto_brackets = { enabled = true } },
+				signature = { enabled = true },
+			},
 			keymap = { preset = "super-tab" },
 		},
 	},
