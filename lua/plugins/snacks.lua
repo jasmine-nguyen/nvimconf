3419b761f14cb94d2ec68c1e094bf09aa69eb7bc
Jasmine Nguyen <maverick@Jasmines-MacBook-Air.local>
2025-02-11 20:28:04 +1100

    Update dashboard keymap

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -29,6 +29,24 @@ return {
 Never trust an atom.
 They make up everything.
 ]],
+				keys = {
+					{ icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+					{ icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+					{
+						icon = " ",
+						key = "g",
+						desc = "Find Text",
+						action = ":lua Snacks.dashboard.pick('live_grep')",
+					},
+					{
+						icon = "󰒲 ",
+						key = "L",
+						desc = "Lazy",
+						action = ":Lazy",
+						enabled = package.loaded.lazy ~= nil,
+					},
+					{ icon = " ", key = "<esc>", desc = "Quit", action = ":qa" },
+				},
 			},
 			sections = {
 				{ section = "header" },
