09290e3605ac6912b51ba8c104b17787a4d9f4aa
Jasmine Nguyen <maverick@Jasmines-MacBook-Air.local>
2025-02-12 21:15:07 +1100

    Add incremental selection in treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,6 +3,15 @@ return {
 	build = ":TSUpdate",
 	main = "nvim-treesitter.configs",
 	opts = {
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "gnn", -- set to `false` to disable one of the mappings
+				node_incremental = "grn",
+				scope_incremental = "grc",
+				node_decremental = "grm",
+			},
+		},
 		ensure_installed = {
 			"apex",
 			"javascript",
