bd08f11a5a9689524d9283b3af24273e1c16edfa
Jasmine Nguyen <maverick@Jasmines-MacBook-Air.local>
2025-02-16 11:36:55 +1100

    Formatting

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -50,7 +50,7 @@ return {
 			-- Apex server setup
 			lspconfig.apex_ls.setup({
 				apex_jar_path = "$Home/lsp/apex-jorje-lsp.jar",
-				apex_enable_semantic_errors = true,   -- Whether to allow Apex Language Server to surface semantic errors
+				apex_enable_semantic_errors = true, -- Whether to allow Apex Language Server to surface semantic errors
 				apex_enable_completion_statistics = true, -- Whether to allow Apex Language Server to collect telemetry on code completion usage
 			})
 
