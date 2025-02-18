5965e7b0682b19b3dde9f9887542da1d3faf7a41
Maverick <maverick@Mavericks-MacBook-Air.local>
2025-02-18 20:43:34 +1100

    Update keymap

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -13,7 +13,7 @@ map(
 map(
 	"n",
 	"<leader>yp",
-	[[:let @+ = expand('%')<cr>:echo "Yanked filepath: " . expand('%')<cr>]],
+	[[:let @+ = expand('%:.')<cr>:echo "Yanked filepath: " . expand('%:.')<cr>]],
 	{ remap = false, silent = true, desc = "yank filepath" }
 )
 
