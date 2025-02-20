8bf209e50e4694379e1c9736d789f7ac07676f88
Maverick <maverick@Mavericks-MacBook-Air.local>
2025-02-20 21:07:58 +1100

    Change tokyonight theme

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -5,7 +5,7 @@ return {
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
 			require("tokyonight").setup({
-				style = "moon",
+				style = "storm",
 				transparent = true,
 				styles = {
 					floats = "transparent",
