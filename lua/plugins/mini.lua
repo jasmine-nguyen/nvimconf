2802e073ffd9568e59cb82746a71119a952d00c2
Maverick <maverick@Mavericks-MacBook-Air.local>
2025-02-22 11:03:07 +1100

    Update mini.pairs config to prevent double quotes in one side is a letter

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -13,10 +13,10 @@ return {
 						i = { "@block.inner", "@conditional.inner", "@loop.inner" },
 					}),
 					f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
-					c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }),  -- class
-					t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" },      -- tags
-					d = { "%f[%d]%d+" },                                                     -- digits
-					e = {                                                                    -- Word with case
+					c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }), -- class
+					t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" }, -- tags
+					d = { "%f[%d]%d+" }, -- digits
+					e = { -- Word with case
 						{
 							"%u[%l%d]+%f[^%l%d]",
 							"%f[%S][%l%d]+%f[^%l%d]",
@@ -25,7 +25,7 @@ return {
 						},
 						"^().*()$",
 					},
-					u = ai.gen_spec.function_call(),                      -- u for "Usage"
+					u = ai.gen_spec.function_call(), -- u for "Usage"
 					U = ai.gen_spec.function_call({ name_pattern = "[%w_]" }), -- without dot in function name
 				},
 			})
@@ -42,6 +42,27 @@ return {
 			require("mini.pairs").setup({
 				mappings = {
 					["<"] = { action = "closeopen", pair = "<>", neigh_pattern = "[^\\].", register = { cr = false } },
+					-- Double quote: Prevent pairing if either side is a letter
+					['"'] = {
+						action = "closeopen",
+						pair = '""',
+						neigh_pattern = "[^%w\\][^%w]",
+						register = { cr = false },
+					},
+					-- Single quote: Prevent pairing if either side is a letter
+					["'"] = {
+						action = "closeopen",
+						pair = "''",
+						neigh_pattern = "[^%w\\][^%w]",
+						register = { cr = false },
+					},
+					-- Backtick: Prevent pairing if either side is a letter
+					["`"] = {
+						action = "closeopen",
+						pair = "``",
+						neigh_pattern = "[^%w\\][^%w]",
+						register = { cr = false },
+					},
 				},
 			})
 		end,
