edde5874b631cfcf90e2628ca0017fc8c3a227db
Maverick <maverick@Mavericks-MacBook-Air.local>
2025-04-03 20:20:24 +1100

    Use gofumt for go formatting

--- lua/plugins/nonels.lua
+++ lua/plugins/nonels.lua
@@ -30,7 +30,7 @@ return {
 					}),
 					null_ls.builtins.diagnostics.yamllint,
 					null_ls.builtins.formatting.buf,
-					null_ls.builtins.formatting.gofmt,
+					null_ls.builtins.formatting.gofumpt,
 					null_ls.builtins.formatting.prettierd.with({
 						filetypes = { "apex", "css", "html", "json", "javascript", "typescript", "yaml" },
 					}),
