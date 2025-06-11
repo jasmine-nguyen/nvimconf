a6275323035272b2449d432524dc9275583641d6
Jas <jasminel.ngn@gmail.com>
2025-06-12 09:38:06 +1000

    Update lsp.lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -60,6 +60,7 @@ return {
 
 			-- Apex server setup
 			lspconfig.apex_ls.setup({
+				apex_jar_path = "~/lsp/apex-jorje-lsp.jar",
 				filetypes = { "apex" },
 				apex_enable_semantic_errors = false,   -- Whether to allow Apex Language Server to surface semantic errors
 				apex_enable_completion_statistics = false, -- Whether to allow Apex Language Server to collect telemetry on code completion usage
