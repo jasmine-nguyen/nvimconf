feb7bcc8c69f9881635d03981ad45b8779d0eebe
Jas <jasminel.ngn@gmail.com>
2025-07-21 11:27:42 +1000

    Update nonels.lua

--- lua/plugins/nonels.lua
+++ lua/plugins/nonels.lua
@@ -1,48 +1,31 @@
 return {
 	{
-		"jay-babu/mason-null-ls.nvim",
-		event = { "BufReadPre", "BufNewFile", "VeryLazy" },
-		dependencies = {
-			"williamboman/mason.nvim",
-			"nvimtools/none-ls.nvim",
-		},
-	},
-	{
 		"nvimtools/none-ls.nvim",
-		event = "VeryLazy",
+		event = { "BufReadPre", "BufNewFile" },
 		dependencies = {
 			"nvim-lua/plenary.nvim",
-			"nvimtools/none-ls-extras.nvim",
 		},
 		config = function()
-			local masonnullls = require("mason-null-ls")
-			masonnullls.setup({
-				ensure_installed = {
-					"gofumpt",
-					"prettier",
-					"prettierd",
-					"protolint",
-					"stylua",
-					"yamlfmt",
-					"yamllint",
-				},
-			})
-			local null_ls = require("null-ls")
+			local none_ls = require("null-ls")
 			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-			null_ls.setup({
+			none_ls.setup({
 				sources = {
-					null_ls.builtins.diagnostics.protolint,
-					null_ls.builtins.diagnostics.golangci_lint.with({
+					none_ls.builtins.diagnostics.protolint,
+					none_ls.builtins.diagnostics.golangci_lint.with({
 						args = { "--allow-parallel-runners" },
 					}),
-					null_ls.builtins.diagnostics.yamllint,
-					null_ls.builtins.formatting.buf,
-					null_ls.builtins.formatting.gofumpt,
-					null_ls.builtins.formatting.prettierd.with({
-						filetypes = { "apex", "css", "html", "json", "javascript", "typescript", "yaml" },
+					none_ls.builtins.diagnostics.yamllint,
+					none_ls.builtins.formatting.buf,
+					none_ls.builtins.formatting.gofumpt,
+					none_ls.builtins.formatting.prettier.with({
+						filetypes = { "apex" },
+						extra_args = { "--plugin=prettier-plugin-apex", "--write" },
 					}),
-					null_ls.builtins.formatting.stylua,
-					null_ls.builtins.formatting.xmllint,
+					none_ls.builtins.formatting.prettierd.with({
+						filetypes = { "css", "html", "json", "javascript", "typescript", "yaml" },
+					}),
+					none_ls.builtins.formatting.stylua,
+					none_ls.builtins.formatting.xmllint,
 				},
 				on_attach = function(client, bufnr)
 					if client.supports_method("textDocument/formatting") then
