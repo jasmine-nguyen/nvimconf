07558633bc424b2aa514cf9563c1e8ff2c885e8f
Jas <jasminel.ngn@gmail.com>
2025-08-07 13:05:15 +1000

    Finally the fucking apex formatting works. Fuck you!!!

--- lua/plugins/nonels.lua
+++ lua/plugins/nonels.lua
@@ -6,25 +6,25 @@ return {
 			"nvim-lua/plenary.nvim",
 		},
 		config = function()
-			local none_ls = require("null-ls")
+			local null_ls = require("null-ls")
 			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-			none_ls.setup({
+			null_ls.setup({
 				sources = {
-					none_ls.builtins.diagnostics.protolint,
-					none_ls.builtins.diagnostics.golangci_lint.with({
+					null_ls.builtins.diagnostics.protolint,
+					null_ls.builtins.diagnostics.golangci_lint.with({
 						args = { "--allow-parallel-runners" },
 					}),
-					none_ls.builtins.diagnostics.yamllint,
-					none_ls.builtins.formatting.buf,
-					none_ls.builtins.formatting.gofumpt,
-					none_ls.builtins.formatting.prettier.with({
+					null_ls.builtins.diagnostics.yamllint,
+					null_ls.builtins.formatting.buf,
+					null_ls.builtins.formatting.gofumpt,
+					null_ls.builtins.formatting.prettier.with({
 						filetypes = { "apex" },
 						extra_args = { "--plugin=prettier-plugin-apex", "--write" },
 					}),
-					none_ls.builtins.formatting.prettierd.with({
+					null_ls.builtins.formatting.prettierd.with({
 						filetypes = { "css", "html", "json", "javascript", "typescript", "yaml" },
 					}),
-					none_ls.builtins.formatting.stylua,
+					null_ls.builtins.formatting.stylua,
 				},
 				on_attach = function(client, bufnr)
 					if client.supports_method("textDocument/formatting") then
@@ -33,7 +33,7 @@ return {
 							group = augroup,
 							buffer = bufnr,
 							callback = function()
-								vim.lsp.buf.format()
+								vim.lsp.buf.format({ bufnr = bufnr, id = client.id, timeout_ms = 2000 })
 							end,
 						})
 					end
