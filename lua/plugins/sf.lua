ff7a3a1a2c5ed4ad5f78b0a4c57602f424d70db3
Jas <jasminel.ngn@gmail.com>
2025-10-03 09:41:28 +1000

    Update sf.lua

--- lua/plugins/sf.lua
+++ lua/plugins/sf.lua
@@ -1,16 +1,17 @@
 return {
-	'xixiaofinland/sf.nvim',
+	"xixiaofinland/sf.nvim",
 
 	dependencies = {
-		'nvim-treesitter/nvim-treesitter',
-		'ibhagwan/fzf-lua', -- no need if you don't use listing metadata feature
+		"nvim-treesitter/nvim-treesitter",
+		"ibhagwan/fzf-lua", -- no need if you don't use listing metadata feature
 	},
 
 	config = function()
-		require('sf').setup() -- Important to call setup() to initialize the plugin!
-		local sf = require('sf')
-		vim.keymap.set('n', '<leader>ss', sf.set_target_org)
-		vim.keymap.set('n', '<leader>sp', sf.save_and_push)
-		vim.keymap.set('n', '<leader>sr', sf.retrieve)
-	end
+		require("sf").setup() -- Important to call setup() to initialize the plugin!
+		local sf = require("sf")
+		vim.keymap.set("n", "<leader>ss", sf.set_target_org, { desc = "set target org" })
+		vim.keymap.set("n", "<leader>sp", sf.save_and_push, { desc = "push current file" })
+		vim.keymap.set("n", "<leader>sr", sf.retrieve, { desc = "retrieve current file" })
+		vim.keymap.set("n", "<leader>sc", sf.copy_apex_name, { desc = "copy apex name" })
+	end,
 }
