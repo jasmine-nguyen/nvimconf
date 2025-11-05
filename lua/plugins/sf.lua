a304ec119f5f81aefbaed2dd234a46e19cd3128c
Jas <jasminel.ngn@gmail.com>
2025-11-05 11:00:34 +1100

    Update sf.lua

--- lua/plugins/sf.lua
+++ lua/plugins/sf.lua
@@ -7,7 +7,9 @@ return {
 	},
 
 	config = function()
-		require("sf").setup() -- Important to call setup() to initialize the plugin!
+		require("sf").setup({
+			fetch_org_list_at_nvim_start = false,	
+		})
 		local sf = require("sf")
 		vim.keymap.set("n", "<leader>ss", sf.set_target_org, { desc = "set target org" })
 		vim.keymap.set("n", "<leader>sp", sf.save_and_push, { desc = "push current file" })
