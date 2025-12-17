f828cdf6cfd7bb9a015084475d0b7fab9addf494
Jasmine Nguyen <maverick@Jasmines-MacBook-Air.local>
2025-12-17 11:22:27 +1100

    Update packages

--- lua/plugins/sf.lua
+++ lua/plugins/sf.lua
@@ -1,6 +1,5 @@
 return {
 	"xixiaofinland/sf.nvim",
-	enabled = false,
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter",
 		"ibhagwan/fzf-lua", -- no need if you don't use listing metadata feature
