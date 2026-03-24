403cc9b952fe57ce5edcfc64d236e42728b8ca0f
Jasmine Nguyen <jasminel.ngn@gmail.com>
2026-03-24 12:24:01 +1100

    Add clipboard for ssh

--- lua/options.lua
+++ lua/options.lua
@@ -90,3 +90,18 @@ vim.schedule(function()
 	vim.opt.clipboard = "unnamedplus"
 	vim.diagnostic.config({ virtual_text = false, virtual_lines = { current_line = true } })
 end)
+
+-- Clipboard for ssh
+if vim.env.SSH_CONNECTION or vim.env.SSH_TTY or vim.env.SSH_CLIENT then
+	vim.g.clipboard = {
+		name = "OSC 52",
+		copy = {
+			["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+			["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+		},
+		paste = {
+			["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+			["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+		},
+	}
+end
