ae26d8ff4694e4ad44d7ac8ecb639effbde00613
Jasmine Nguyen <jasminel.ngn@gmail.com>
2026-05-31 21:47:27 +1000

    chore: fix apex treesitter issue

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -48,7 +48,7 @@ return {
 
 			-- Apex server setup
 			vim.lsp.config("apex_ls", {
-				apex_jar_path = "/Users/maverick/.local/share/nvim/mason/share/apex-language-server/apex-jorje-lsp.jar",
+				apex_jar_path = vim.fn.stdpath("data") .. "/mason/share/apex-language-server/apex-jorje-lsp.jar",
 				apex_enable_semantic_errors = false, -- Whether to allow Apex Language Server to surface semantic errors
 				apex_enable_completion_statistics = false, -- Whether to allow Apex Language Server to collect telemetry on code completion usage
 			})
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,9 +3,11 @@ return {
 	branch = "main",
 	event = { "BufReadPost", "BufNewFile" },
 	build = ":TSUpdate",
-	main = "nvim-treesitter",
-	opts = {
-		ensure_installed = {
+	config = function()
+		local ts = require("nvim-treesitter")
+		ts.setup()
+
+		local parsers = {
 			"apex",
 			"dockerfile",
 			"javascript",
@@ -23,7 +25,37 @@ return {
 			"python",
 			"sql",
 			"yaml",
-		},
-		auto_install = true,
-	},
+		}
+
+		local installed = ts.get_installed("parsers")
+		local to_install = {}
+		for _, p in ipairs(parsers) do
+			if not vim.tbl_contains(installed, p) then
+				table.insert(to_install, p)
+			end
+		end
+		if #to_install > 0 then
+			ts.install(to_install)
+		end
+
+		local filetypes = {}
+		for _, p in ipairs(parsers) do
+			local fts = vim.treesitter.language.get_filetypes(p)
+			for _, ft in ipairs(fts) do
+				table.insert(filetypes, ft)
+			end
+		end
+
+		vim.api.nvim_create_autocmd("FileType", {
+			group = vim.api.nvim_create_augroup("treesitter-start", { clear = true }),
+			pattern = filetypes,
+			callback = function(args)
+				local lang = vim.treesitter.language.get_lang(vim.bo[args.buf].filetype)
+				if lang and vim.treesitter.language.add(lang) then
+					vim.treesitter.start(args.buf, lang)
+					vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end
+			end,
+		})
+	end,
 }
